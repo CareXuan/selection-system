0b9a4613a62ab31e78e9bedce66a7ec17d59c525
yixuann.liu <yixuann.liu@ushow.media>
2020-01-20 15:00:49 +0800

    添加css，js文件并测试controller配置

---
+++ controllers/system/TestLiuController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 刘懿萱
+ * Date: 2020/1/20
+ * Time: 14:35
+ */
+
+namespace app\controllers\system;
+
+use yii\console\Controller;
+
+class TestLiuController extends Controller
+{
+  public function actionTest(){
+//    return $this->render();
+    echo "test";
+  }
+}
